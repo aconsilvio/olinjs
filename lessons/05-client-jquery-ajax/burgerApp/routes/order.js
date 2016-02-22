48333f0b95017a4758068bd94486fb41d0903ad8
Sarah Walters <sarah.walters@students.olin.edu>
2016-02-22 17:34:18 -0500

    Homework 5 feedback

--- lessons/05-client-jquery-ajax/burgerApp/routes/order.js
+++ lessons/05-client-jquery-ajax/burgerApp/routes/order.js
@@ -19,47 +19,49 @@ var order = {};
 function errorHandler(err, req, res, next) {
   res.status(500);
   res.render('error', { error: err });
+  // You can even chain these--
+  // res.status(500).render('error', { error: err });
 }
 
 order.listAll = function(req, res){
-	var disabledList = [];
+	var disabledList = []; // is this ever used? If not, best to clean it up
 	Ingredient.find({}, function(err, ingredientsList){
 
-		if (err) errorHandler(err,req,res);
-			
-		// ingredientsList.forEach(function (ingredientItem){});
+		if (err) errorHandler(err,req,res); // I love that you abstracted your error handler out! Very clean. I'm stealing this :)
 
 		ingredientsList.forEach(function (ingredient){
 			if(ingredient.outOfStock === true){
-				ingredient['disabled'] = 'disabled';
+				// You should be able to use dot notation (ingredient.disabled) instead of bracket notation -- though it looks like `disabled` might be a JavaScript keyword
+				ingredient['disabled'] = 'disabled'; // Seems redundant to me -- do 'disabled' and 'outOfStock' mean the same thing?
 			} else{
 				ingredient['disabled'] = '';
 			};
 		});
 		res.render('order', {list: ingredientsList});
-	});	
+	});
 };
 
 order.orderBurger = function(req, res){
 	var ingredients = req.body.ingreds;
-	console.log(ingredients)
 	var ingredientsInBurger = [];
 	var price = 0;
 	Ingredient.find({outOfStock: false}, function(err, ingredientsList){
+		// Why do you need to check that all ingredientsInBurger are in the ingredientsList?
+		// (I think that's what this nested loop does).
+		// Maybe better to prevent users from selecting ingredients which are out of stock on the client side?
 		for(var i = 0; i<ingredients.length; i++){
 			for(var j = 0; j < ingredientsList.length; j++){
 				if(ingredients[i] === ingredientsList[j].name){
 					ingredientsInBurger.push(ingredientsList[j])
 					price = price + ingredientsList[j].price;
-
 				}
 			}
 		}
 		var newOrder = new Order({ingredients:ingredientsInBurger, price:price.toFixed(2)});
 		newOrder.save(function(err, order){
 			res.json(order);
-		})
-	})
+		});
+	});
 };
 
 module.exports = order;
